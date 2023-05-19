33ccefc341d39ec9385d0665aa2ac9d9934e4e52
Satyam Chourasia <satyam.20ch@gmail.com>
2023-05-19 20:28:15 +0530

    build Responive UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:expense/widget/expenses.dart';
 
 var kColorScheme = ColorScheme.fromSeed(
@@ -11,6 +12,11 @@ var kDarkColorScheme = ColorScheme.fromSeed(
 );
 
 void main() {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // SystemChrome.setPreferredOrientations([
+  //   DeviceOrientation.portraitUp,
+  // ]).then(
+  //   (fn) =>
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
